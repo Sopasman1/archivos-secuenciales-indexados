ded601d0c8c03103999501f64f270d0964a89b32
Jezuz <Jezuz@JESUSALBERTO5>
2025-02-12 00:33:32 -0600

    Add project files.

---
+++ archivos secuenciales indexados/Form1.cs
@@ -0,0 +1,10 @@
+namespace archivos_secuenciales_indexados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
